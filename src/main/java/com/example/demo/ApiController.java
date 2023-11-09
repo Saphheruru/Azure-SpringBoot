b7c3320ffd12b70b30a939d07c9e3004fc6f6517
Saphheruru <timothyjosceff.m@digipen.edu>
2023-11-09 18:54:01 +0800

    added another resonse

--- src/main/java/com/example/demo/ApiController.java
+++ src/main/java/com/example/demo/ApiController.java
@@ -43,8 +43,8 @@ public class ApiController {
         return ResponseEntity.ok("Received (POST): " + requestBody.getdbToken());
     }
     @GetMapping("/message")
-	public String message(){
-		return "Congrats ! your application deployed successfully in Azure Platform. !";
+	public ResponseEntity<String> message(){
+		return ResponseEntity.ok("Congrats ! your application deployed successfully in Azure Platform. !");
 	}
 
 
